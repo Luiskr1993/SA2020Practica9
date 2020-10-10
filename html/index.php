5557b34b9cfad59294b8fb4ae08bf036be398aca
Luiskr1993 <luiskr1993@gmail.com>
2020-10-09 22:34:14 -0600

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DOCKER | PHP PRUEBA</title>
+</head>
+<body>
+    <div style="font-size: 60px; text-align:center;">
+        <p>Prueba de Docker con PHP</p>
+        <p>Intento de creaciòn de servicio web con php</p>
+    </div>
+</body>
+</html>
